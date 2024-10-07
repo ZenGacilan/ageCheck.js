1803485737bbf0f6358d7239f6c0706029cf078b
ZenGacilan <potapetekau@gmail.com>
2024-10-07 09:43:18 +0800

    initial commit

---
+++ add.js
@@ -0,0 +1,15 @@
+// ageCheck.js
+
+function checkAge() {
+  const age = parseInt(prompt("20:"));
+
+  if (age < 13) {
+    console.log("You are a child.");
+  } else if (age >= 13 && age <= 19) {
+    console.log("You are a teenager.");
+  } else {
+    console.log("You are an adult.");
+  }
+}
+
+checkAge();
